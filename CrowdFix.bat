31ee8ba5f4177fb995312f164ed9ef33063e85d4
Emir Ramić <69081472+ramice@users.noreply.github.com>
2024-07-19 13:46:13 +0200

    Update CrowdFix.bat

--- CrowdFix.bat
+++ CrowdFix.bat
@@ -1,17 +1,14 @@
 @echo off
 title CrowdFix by eramic@hotmail.com
-
 :: BY: www.linkedin.com/in/eramic
 color 0A
-
 echo =============================================
 echo           Welcome to CrowdFix
-echo          by eramic@hotmail.com
 echo for those affected by Crowdstrike Falcon Senzor Update problem
 echo          this is not official fix
 echo =============================================
 echo.
 timeout /t 3 /nobreak >nul
-cd %windir%
+cd %windir%\System32\drivers\Crowdstrike
 del /f /q C-00000291*.sys
 shutdown /r /t 1
